ec8b636bb1c9e87aaf0c8ae4e740ce1332b3f72a
Xymen144 <xymen.martinez.i25@gmail.com>
2026-04-27 15:34:34 -0300

    Se agrego funcion en UsuarioService para ver el cartel de rol desde el front

--- ServicesNegocio/UsuarioService.cs
+++ ServicesNegocio/UsuarioService.cs
@@ -140,5 +140,12 @@ namespace ServicesNegocio
         {
             return _context.Usuarios.Include(u => u.Rol).ToList();
         }
+        // Agrega esto al final de tu UsuarioService.cs
+        public Usuario ObtenerPorId(int id)
+        {
+            return _context.Usuarios
+                .Include(u => u.Rol)
+                .FirstOrDefault(u => u.IdRol == id);
+        }
     }
 }
