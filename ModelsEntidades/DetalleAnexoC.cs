a4df29c5e28745029a0caa71670c771ea63f4b08
Xymen144 <xymen.martinez.i25@gmail.com>
2026-09-19 02:22:29 -0300

    MAPEO DE ENTIDAD DETALLE ANEXO C

--- ModelsEntidades/DetalleAnexoC.cs
+++ ModelsEntidades/DetalleAnexoC.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace ModelsEntidades
 {
-    
+
     [Table("detalles_anexo_c")]
     public class DetalleAnexoC
     {
@@ -27,9 +27,33 @@ namespace ModelsEntidades
         [StringLength(50)]
         public string NumeroNotaPermiso { get; set; }
 
+        // --- DATOS PERSONALES FALTANTES ---
+        [Column("apellido")]
+        [StringLength(100)]
+        public string Apellido { get; set; }
+
+        [Column("nombres")]
+        [StringLength(100)]
+        public string Nombres { get; set; }
+
+        [Column("dni_pasaporte")]
+        [StringLength(30)]
+        public string DniPasaporte { get; set; }
+
         [Column("estado_civil")]
         [StringLength(30)]
         public string EstadoCivil { get; set; }
+
+        [Column("lugar_nacimiento")]
+        [StringLength(100)]
+        public string LugarNacimiento { get; set; }
+
+        [Column("fecha_nacimiento")]
+        public DateTime FechaNacimiento { get; set; }
+
+        [Column("cargo_funcion")]
+        [StringLength(100)]
+        public string CargoFuncion { get; set; }
 
         // Domicilio separado
         [Column("calle")][StringLength(100)] public string Calle { get; set; }
@@ -44,11 +68,41 @@ namespace ModelsEntidades
         [Column("tel_laboral")][StringLength(30)] public string TelLaboral { get; set; }
         [Column("mail")][StringLength(100)] public string Mail { get; set; }
 
-        // Emergencia y adicionales
+        // Emergencia y adicionales (OACI Doc. 8973)
         [Column("grupo_sanguineo")][StringLength(10)] public string GrupoSanguineo { get; set; }
         [Column("factor_rh")][StringLength(10)] public string FactorRh { get; set; }
         [Column("enfermedades_alergias")] public string EnfermedadesAlergias { get; set; }
-        [Column("datos_socorrista_conductor")] public string DatosSocorristaConductor { get; set; }
+
+        [Column("socorrista")]
+        public bool Socorrista { get; set; }
+
+        [Column("conductor")]
+        public bool Conductor { get; set; }
+
+        // --- MATRIZ DE SECTORES Y JUSTIFICACIONES ---
+        [Column("sector_1")] public bool Sector1 { get; set; }
+        [Column("sector_1_justif")] public string Sector1Justif { get; set; }
+
+        [Column("sector_2")] public bool Sector2 { get; set; }
+        [Column("sector_2_justif")] public string Sector2Justif { get; set; }
+
+        [Column("sector_3")] public bool Sector3 { get; set; }
+        [Column("sector_3_justif")] public string Sector3Justif { get; set; }
+
+        [Column("sector_4")] public bool Sector4 { get; set; }
+        [Column("sector_4_justif")] public string Sector4Justif { get; set; }
+
+        [Column("sector_5")] public bool Sector5 { get; set; }
+        [Column("sector_5_justif")] public string Sector5Justif { get; set; }
+
+        [Column("sector_6")] public bool Sector6 { get; set; }
+        [Column("sector_6_justif")] public string Sector6Justif { get; set; }
+
+        [Column("sector_7")] public bool Sector7 { get; set; }
+        [Column("sector_7_justif")] public string Sector7Justif { get; set; }
+
+        [Column("justif_sna_regionales")]
+        public string JustifSnaRegionales { get; set; }
 
         // Relación de navegación con Solicitud
         [ForeignKey("IdSolicitud")]
